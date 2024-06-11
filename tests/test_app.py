cc2af652a7c750491dc80f694d2394ddd2c3da66
Brian Nguyen <79072222+Brian1226@users.noreply.github.com>
2024-06-11 16:41:22 -0700

    fixed arguments for function under test

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,7 @@ def test_get_password(mock_get):
     mock_response.status_code = 200 # simulates successful HTTP response
     mock_response.json.return_value = {'random_password': "X4s$g?3F"} # simulates JSON payload returned by API
     mock_get.return_value = mock_response # any call to 'requests.get' within this test returns 'mock_response' object
-    data = app.get_password(5) # call function under test
+    data = app.get_password(8) # call function under test
     assert data == "X4s$g?3F" # asserts that the result from function under test is equal to specified string
 
 
@@ -25,4 +25,4 @@ def test_get_password_error(mock_get):
     mock_response.status_code = 400
     mock_get.return_value = mock_response
     with pytest.raises(requests.HTTPError):
-        app.get_password(5)
+        app.get_password(8)
