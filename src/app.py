a7600dfa02665b40a1d1cdb8bb50948f08489900
Brian Nguyen <nguyenbrian1226@gmail.com>
2024-09-09 16:05:36 -0700

    improved unsuccessful API response handling

--- src/app.py
+++ src/app.py
@@ -7,16 +7,19 @@ load_dotenv()
 API_URL = 'https://api.api-ninjas.com/v1/passwordgenerator'
 API_KEY = os.getenv('API_KEY')
 header = {'X-Api-Key': API_KEY}
+print(API_KEY)
 
 def get_password(length):
     param = {'length' : length}
     response = requests.get(API_URL, headers=header, params=param)
+    if response.status_code != requests.codes.ok:
+        print(f"Response Status Code: {response.status_code}")
+        print(f"Response Content: {response.text}") 
+    response.raise_for_status()
+    data = response.json()
+    return data['random_password']
 
-    if response.status_code == requests.codes.ok:
-        data = response.json()
-        return data['random_password']
-
-    raise requests.HTTPError
 
 if __name__ == '__main__':
     print(get_password(8))
+    
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,24 @@
 import pytest
 import unittest.mock as mock
 import requests
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import src.app as app
 
 
-@mock.patch('requests.get') # replaces/intercepts 'requests.get' with 'mock_get'
+@mock.patch('src.app.requests.get')                                     # replaces'requests.get' with 'mock_get', and passes that mock object as argument
 def test_get_password(mock_get):
-    mock_response = mock.Mock() # creates mock response object
-    mock_response.status_code = 200 # simulates successful HTTP response
-    mock_response.json.return_value = {'random_password': "X4s$g?3F"} # simulates JSON payload returned by API
-    mock_get.return_value = mock_response # any call to 'requests.get' within this test returns 'mock_response' object
-    data = app.get_password(8) # call function under test
-    assert data == "X4s$g?3F" # asserts that the result from function under test is equal to specified string
+    mock_response = mock.Mock()                                         # creates mock response object
+    mock_response.status_code = requests.codes.ok                       # simulates successful HTTP response
+    mock_response.json.return_value = {'random_password': "X4s$g?3F"}   # simulates JSON payload returned by API
+    mock_get.return_value = mock_response                               # any call to 'requests.get' within this test returns 'mock_response' object
+    data = app.get_password(8)                                          # call function under test
+    assert data == "X4s$g?3F"                                           # asserts that the result from function under test is equal to specified string
 
 
-@mock.patch('requests.get')
+@mock.patch('src.app.requests.get')
 def test_get_password_error(mock_get):
     mock_response = mock.Mock()
-    mock_response.status_code = 400
-    mock_get.return_value = mock_response
-    with pytest.raises(requests.HTTPError):
+    mock_response.status_code = requests.codes.bad_request              # simulates unsuccessful HTTP response
+    mock_response.raise_for_status.side_effect = requests.HTTPError     # simulates raising HTTPError when raise_for_status is called
+    mock_get.return_value = mock_response                               
+    with pytest.raises(requests.HTTPError):                             # assert that function under test raises HTTPError
         app.get_password(8)
